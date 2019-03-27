1c6cd41838187276cf2b919bcfde57c74945fa99
bmacd43 <48831834+bmacd43@users.noreply.github.com>
2019-03-27 12:19:02 +0000

    Add files via upload
    email program

---
+++ gmail.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import smtplib
+
+
+def email(gmail_account, password, recipient, subject, message):
+    content = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (gmail_account, recipient, subject, message)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_account, password)
+    server.sendmail(gmail_account, recipient, content)
+    server.close()
+
+gmail = "roomnineteenclass@gmail.com"
+password = "muppetational"
+recipient = "rebeccamacdonald2018@mallowcollege.email"
+
+
+email(gmail, password, recipient, "Warning", "This is a warning from your computer about ")
