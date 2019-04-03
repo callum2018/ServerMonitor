1367d82b2001b05d0df65eb6ea3a3f601f52aa77
Daniel <45067018+EternalParsley@users.noreply.github.com>
2019-04-03 13:47:51 +0100

    Update cpu_usage.py

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import time
+
 import psutil
 
-while True:
-    current_CPU_Usage = psutil.cpu_percent(interval=1)
-    print("Current %CPU Usage is: ", current_CPU_Usage)
-    time.sleep(1)
+
+def cpu_usage(usage="usge"):
+    if usage == "usage":
+        return psutil.cpu_percent(interval=1)
+
