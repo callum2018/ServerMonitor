2015ade32109f47ae45653a7300f078f385fc756
Daniel <45067018+EternalParsley@users.noreply.github.com>
2019-04-03 13:15:16 +0100

    Update memoryusage.py

--- memoryusage.py
+++ memoryusage.py
@@ -10,12 +10,17 @@ def memory_usage(memusage="all"):
     if memusage == "all":
         return usage
     elif memusage == "free":
-        return print("Free memory GB: ", round(psutil.virtual_memory().free / (1024.0 ** 3), 2))
+        return round(psutil.virtual_memory().free / (1024.0 ** 3), 2)
     elif memusage == "total":
-        return print("Total memory in GB: ", round(psutil.virtual_memory().total / (1024.0 ** 3), 2))
+        return round(psutil.virtual_memory().total / (1024.0 ** 3), 2)
     elif memusage == "available":
-        return print("Available memory in GB: ", round(psutil.virtual_memory().available / (1024.0 ** 3), 2))
+        return round(psutil.virtual_memory().available / (1024.0 ** 3), 2)
     elif memusage == "percent":
-        return print("Total Percent Used: ", psutil.virtual_memory().percent)
+        return psutil.virtual_memory().percent
     elif memusage == "used":
-        return print("Used memory in GB: ", round(psutil.virtual_memory().used / (1024.0 ** 3), 2))
+        return round(psutil.virtual_memory().used / (1024.0 ** 3), 2)
+
+
+#print(memory_usage("all"))
+
+#print(memory_usage("total"))
