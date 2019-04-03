4ab8d6ec5d055341ca2b1ecd0e37a2fee044cf83
Eoin <48831874+eoinwebb2018@users.noreply.github.com>
2019-04-03 13:15:15 +0100

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,19 @@
+from subprocess import check_output
+
+
+def system_time():
+    """Returns the Time of the day"""
+    output = check_output(["date"]).decode("utf-8").split()
+    time = output[3]
+    return time
+
+
+def system_date():
+    """Returns the date"""
+    output = check_output(["date"]).decode("utf-8").split()
+    date = output[2] + " " + output[1] + " " + output[5]
+    return date
+
+
+print("The date is", (system_date()))
+print("The time is", system_time())
