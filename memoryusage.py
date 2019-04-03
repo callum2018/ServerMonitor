4aa449ec8fb0369df0b4166d7306b56f5bfa0e90
Daniel <45067018+EternalParsley@users.noreply.github.com>
2019-04-03 12:45:28 +0100

    Update memoryusage.py

--- memoryusage.py
+++ memoryusage.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 
-
 import psutil
 
 usage = psutil.virtual_memory()
 
 
-def memory_usage(memusage="mem"):
-    if memusage == "mem":
+def memory_usage(memusage="all"):
+    if memusage == "all":
         return usage
+    elif memusage == "free":
+        return psutil.virtual_memory().free
+    elif memusage == "total":
+        return psutil.virtual_memory().total
+    elif memusage == "avaiable":
+        return psutil.virtual_memory().available
+    elif memusage == "percent":
+        return psutil.virtual_memory().percent
+    elif memusage == "used":
+        return psutil.virtual_memory().used
