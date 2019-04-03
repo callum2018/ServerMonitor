1646314b53e21a26ccba0b1379e97517ff6d5600
bmacd43 <48831834+bmacd43@users.noreply.github.com>
2019-04-03 13:25:08 +0100

    Add files via upload

---
+++ rebecca.py
@@ -0,0 +1,61 @@
+"""Module to return uptime information."""
+
+from subprocess import check_output
+import socket
+import smtplib
+
+def up_time():
+    """Returns the uptime."""
+    output = " ".join(check_output(["uptime", "-p"]).decode("utf-8").split()[1:])
+    return output
+
+def load():
+    """Returns the system load."""
+    output = check_output(["uptime"]).decode("utf-8").split()
+    load_1 = output[-3]
+    load_2 = output[-2]
+    load_3 = output[-1]
+    load_average = load_1 + load_2 + load_3
+    return load_average
+
+def user():
+    """Returns the number of users."""
+    output = check_output(["uptime"]).decode("utf-8").split()
+    user = output[-7]
+    return user
+
+
+def time():
+    """Returns the time."""
+    output = check_output(["uptime"]).decode("utf-8").split()
+    time = output[0]
+    return time
+
+def get_hostname():
+
+    name = socket.gethostname()
+    return name
+
+def email(gmail_account, password, recipient, subject, message):
+    content = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (gmail_account, recipient, subject, message)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_account, password)
+    server.sendmail(gmail_account, recipient, content)
+    server.close()
+
+gmail = "roomnineteenclass@gmail.com"
+password = "************"
+recipient = "roomnineteenclass@gmail.com"
+
+
+email(gmail, password, recipient, "", "This is a warning from your computer about the CPU temperature.")
+
+
+
+filename = "cpu.log"
+gmail = "roomnineteenclass@gmail.com"
+password = "***********"
+recipient = "roomnineteenclass@gmail.com"
