004525834cf971dd5636b0b6bf083d2c43589837
adam9988 <48989586+adam9988@users.noreply.github.com>
2019-04-08 11:52:07 +0100

    Update ping_adam.py

--- ping_adam.py
+++ ping_adam.py
@@ -1,12 +1,17 @@
-
 import os
 
 
-machine = "london.local"
-ping = os.system("ping -c 1 " + machine)
-print()
+def pingtest(machine):
+    """
+     The program pings a chosen website or server, the '-c 1' means that it will ping just once. and the '> /dev/null' sends away the unwanted results.
+     If we get an exit code of 0, it means that the site or server is working properly, otherwise it indicates that it's not running as intended.
+    """
+    ping = os.system("ping -c 1 " + machine + " > /dev/null" )
+    #print(ping)
+    if ping == 0:
+        return machine + 'is running without any faults.'
+    else:
+        return machine + 'is not running.'
 
-if ping == 0:
-  print (machine, 'is running without any complications.')
-else:
-  print (machine, 'is not running.')
+
+#print(pingtest("mallowcollege.ie"))
