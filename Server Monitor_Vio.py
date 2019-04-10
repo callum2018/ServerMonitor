7e80bff6e9c8289e2ff78fb694c894017eda150a
Vio Ismael Pajanustan <48989897+Vio2319@users.noreply.github.com>
2019-04-10 13:07:42 +0100

    Update and rename Bell+sys mess Vio.py to Server Monitor_Vio.py

--- Server Monitor_Vio.py
+++ Server Monitor_Vio.py
@@ -4,24 +4,21 @@ Can be combined within codes that need an alarm
 """
 import sys
 import time
+import easygui
 
 def bell():
     sys.stdout.write('\a')
     sys.stdout.flush()
 
-
 def alarm_bell():
     """alarms for 2 sec"""
-    for times in range(100):
+    for times in range(10):
         bell()
         time.sleep(.02)
 
-alarm_bell()
+def dialog(text,title):
+    """Will Output a message."""
+    easygui.msgbox(text, title=title)
 
 
-
-#!/usr/bin/env python3
-
-"""Will Output a message."""
-import easygui
-easygui.msgbox("Warning Temp Is High!!! Get Out!!! It will explode!!!", title="Warning!!!!")
+#dialog("hello", "Information")
