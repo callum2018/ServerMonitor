b06441c3ab62dc68cf031ee7125a7eb41fb97b1c
MatthewFinn99 <finnmatthew2@gmail.com>
2019-04-10 13:08:52 +0100

    Create servermonitormatthew.py

---
+++ servermonitormatthew.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+needs vext.gi and playsound
+
+"""
+
+from gtts import gTTS
+from playsound import playsound
+
+
+def speak(text):
+    filename = "speech.mp3"
+    talk = gTTS(text)
+    talk.save(filename)
+    playsound(filename)
