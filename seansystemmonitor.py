127a657c8465ee7f85170136e88987dfd6f226dd
seano2018mallow <48912379+seano2018mallow@users.noreply.github.com>
2019-04-11 13:15:34 +0100

    Add files via upload

---
+++ seansystemmonitor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+This program monitors the server and outputs to screen, to HTML.
+"""
+
+from rebecca import *
+from time import sleep
+from sean import *
+
+#from rain import *
+
+interval = 5 * 60  # 5 minutes in seconds
+
+while True:
+    message = "SERVER MONITOR 0.1\n"
+    message += "\nUptime is " + up_time() # rebecca
+    message += "\nThere are " + str(users()) + " users and " + identity() + " is current user"
+    html = "<html><head><title>Server Monitor Report</title></head><body>"
+    html += "<h1>SERVER MONITOR 0.1</h1><hr>"
+    html += "<p><b>Uptime</b> is " + up_time() # rebecca
+    # add your HTML contribution here
+    html += "</body></html>"
+
+    print(message)
+    report = open("servermonitor.html", "w")
+    print(html, file=report)
+    report.close()
+
+    # add any testing function here
+    sleep(interval)
