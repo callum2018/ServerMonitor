149e9b285fae5ba8d46983a88856b5062e58e0dd
MatthewFinn99 <finnmatthew2@gmail.com>
2019-04-11 14:53:33 +0100

    Update matthew.py

--- matthew.py
+++ matthew.py
@@ -2,22 +2,17 @@
 
 """
 needs vext.gi and playsound
-
+Playsound: This imports playsound from the python files which allows the mp3 file to play 
+through terminal without manually running it, playsound also needs vext.gi to work properly.
+gTTS : This imports google text-to-speech from the python files.
 """
 
 from gtts import gTTS
 from playsound import playsound
-"""Playsound: This imports playsound from the python files which allows the mp3 file to play 
-through terminal without manually running it."""
-
-
-"""gTTS : This imports google text-to-speech from the python files."""
 
 def speak(text):
-        """Speak is the function and what it does is it runs google text-to-speech which will say whatever the
-    message is that comes up on the screen when we run servermonitor.py"""
-    filename = "speech.mp3"  """This line here saves a file as speech.mp3 which is what plays when we run
-     servermonitor."""
+        """This function runs google text to speech and sdays whatever the text is."""
+    filename = "speech.mp3"
     talk = gTTS(text)
-    talk.save(filename)
+    talk.save(filename) """Saves a file as speech.mp3"""
     playsound(filename)
